933898fd65c375d7c3a9c15ea168c0ce6968e651
Robert Ignasiak <tori@robert-i.com>
2017-04-24 10:12:59 +0200

    TimeZone fix

--- Auto Stop Race/Extension/Date.swift
+++ Auto Stop Race/Extension/Date.swift
@@ -9,10 +9,18 @@
 import Foundation
 
 extension Date {
+    
+    func toLocalTime() -> Date {
+        let timeZone = NSTimeZone.local
+        let seconds : TimeInterval = Double(timeZone.secondsFromGMT(for:self))
+        let localDate = Date(timeInterval: seconds, since: self)
+        return localDate
+    }
+    
     func toString(withFormat: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = withFormat
         dateFormatter.locale = Locale(identifier: "pl")
-        return dateFormatter.string(from: self)
+        return dateFormatter.string(from: self.toLocalTime())
     }
 }
