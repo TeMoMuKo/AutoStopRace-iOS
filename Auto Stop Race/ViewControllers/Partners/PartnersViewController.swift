3bcecd2bcf319cb5399dec0e6d8a32f29283cc31
Robert Ignasiak <robert.ignasiak@intive.com>
2018-04-16 01:11:11 +0200

    Update partners screen

--- Auto Stop Race/ViewControllers/Partners/PartnersViewController.swift
+++ Auto Stop Race/ViewControllers/Partners/PartnersViewController.swift
@@ -12,26 +12,64 @@ class PartnersViewController: UIViewControllerWithMenu, UICollectionViewDelegate
     
     let partners = [
         Partner(cellType: .text, value: NSLocalizedString("strategic_partners", comment: "")),
-        Partner(cellType: .image, value: "strategic_partner_kaufland"),
-        Partner(cellType: .image, value: "strategic_partner_wro"),
+        Partner(cellType: .image, value: "strategic/kaufland"),
+        Partner(cellType: .image, value: "strategic/auto24"),
         Partner(cellType: .text, value: NSLocalizedString("gold_partners", comment: "")),
-        Partner(cellType: .image, value: "gold_partner_tarczynski"),
-        Partner(cellType: .image, value: "gold_partner_grzeski"),
-        Partner(cellType: .image, value: "gold_partner_profi_lingua"),
-        Partner(cellType: .image, value: "gold_partner_akvo_active"),
-        Partner(cellType: .image, value: "gold_partner_capoeira"),
-        Partner(cellType: .image, value: "gold_partner_wachtigall"),
-        Partner(cellType: .image, value: "gold_partner_cafe_borowka"),
+        Partner(cellType: .image, value: "gold/wachitgall"),
+        Partner(cellType: .image, value: "gold/hatchi"),
+        Partner(cellType: .image, value: "gold/mamut"),
+        Partner(cellType: .image, value: "gold/akvo"),
+        Partner(cellType: .image, value: "gold/luba"),
+        Partner(cellType: .image, value: "gold/sygnet"),
         Partner(cellType: .text, value: NSLocalizedString("silver_partners", comment: "")),
-        Partner(cellType: .image, value: "silver_partner_profi"),
-        Partner(cellType: .image, value: "silver_partner_71zona"),
-        Partner(cellType: .image, value: "silver_partner_meray"),
-        Partner(cellType: .image, value: "silver_partner_hydropolis"),
-        Partner(cellType: .image, value: "silver_partner_krav_maga"),
-        Partner(cellType: .image, value: "silver_partner_lirene"),
-        Partner(cellType: .image, value: "silver_partner_motyla_noga"),
-        Partner(cellType: .image, value: "silver_partner_beactive"),
-        Partner(cellType: .image, value: "silver_partner_el_sueno")
+        Partner(cellType: .image, value: "silver/profi"),
+        Partner(cellType: .image, value: "silver/lovex"),
+        Partner(cellType: .image, value: "silver/snowz"),
+        Partner(cellType: .image, value: "silver/kravmaga"),
+        Partner(cellType: .image, value: "silver/burgerking"),
+        Partner(cellType: .image, value: "silver/narny"),
+        Partner(cellType: .text, value: NSLocalizedString("bronze_partners", comment: "")),
+        Partner(cellType: .image, value: "bronze/sonko"),
+        Partner(cellType: .image, value: "bronze/irenki"),
+        Partner(cellType: .image, value: "bronze/sandmix"),
+        Partner(cellType: .image, value: "bronze/funsurf"),
+        Partner(cellType: .image, value: "bronze/sjos"),
+        Partner(cellType: .image, value: "bronze/spocket"),
+        Partner(cellType: .image, value: "bronze/unicarcapoeira"),
+        Partner(cellType: .image, value: "bronze/setenta"),
+        Partner(cellType: .image, value: "bronze/gorczanska"),
+        Partner(cellType: .image, value: "bronze/yourtemporarytattoos"),
+        Partner(cellType: .image, value: "bronze/elsueno"),
+        Partner(cellType: .image, value: "bronze/inkblink"),
+        Partner(cellType: .text, value: NSLocalizedString("supporting_partners", comment: "")),
+        Partner(cellType: .image, value: "supporting/domekzkart"),
+        Partner(cellType: .image, value: "supporting/albi"),
+        Partner(cellType: .image, value: "supporting/lereve"),
+        Partner(cellType: .image, value: "supporting/wydrukujemyto"),
+        Partner(cellType: .image, value: "supporting/bezdroza"),
+        Partner(cellType: .image, value: "supporting/ksiegarniapodroznika"),
+        Partner(cellType: .image, value: "supporting/gojump"),
+        Partner(cellType: .image, value: "supporting/cudawianki"),
+        Partner(cellType: .image, value: "supporting/tkalniazagadek"),
+        Partner(cellType: .image, value: "supporting/mobicafe"),
+        Partner(cellType: .image, value: "supporting/hdprint"),
+        Partner(cellType: .image, value: "supporting/crashball"),
+        Partner(cellType: .text, value: NSLocalizedString("honorary_partners", comment: "")),
+        Partner(cellType: .image, value: "honorary/uewroc"),
+        Partner(cellType: .image, value: "honorary/samorzad"),
+        Partner(cellType: .text, value: NSLocalizedString("technical_partners", comment: "")),
+        Partner(cellType: .image, value: "technical/smscenter"),
+        Partner(cellType: .image, value: "technical/sendingo"),
+        Partner(cellType: .image, value: "technical/damianmekal"),
+        Partner(cellType: .image, value: "technical/paramed"),
+        Partner(cellType: .image, value: "technical/mavmedia"),
+        Partner(cellType: .text, value: NSLocalizedString("talk_about_us", comment: "")),
+        Partner(cellType: .image, value: "talk_about_us/nationalgeographic"),
+        Partner(cellType: .image, value: "talk_about_us/meloradio"),
+        Partner(cellType: .image, value: "talk_about_us/luz"),
+        Partner(cellType: .image, value: "talk_about_us/radiogra"),
+        Partner(cellType: .text, value: NSLocalizedString("media_partnet", comment: "")),
+        Partner(cellType: .image, value: "media/eska")
     ]
     
     let textCellHeight: CGFloat = 100
@@ -46,25 +84,25 @@ class PartnersViewController: UIViewControllerWithMenu, UICollectionViewDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.register(PartnerImageCell.self, forCellWithReuseIdentifier: PartnerImageCell.Identifier)
-        collectionView.register(PartnerTextCell.self, forCellWithReuseIdentifier: PartnerTextCell.Identifier)
 
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
         setupNavigationBarTitle()
         setupCollectionView()
         
         view.backgroundColor = UIColor.white
     }
     
-    func setupNavigationBarTitle() {
+    private func setupNavigationBarTitle() {
         let titleLabel = navigationItem.titleView as! UILabel
         titleLabel.text = NSLocalizedString("title_partners", comment: "")
     }
     
-    func setupCollectionView() {
+    private func setupCollectionView() {
+        collectionView.register(PartnerImageCell.self, forCellWithReuseIdentifier: PartnerImageCell.Identifier)
+        collectionView.register(PartnerTextCell.self, forCellWithReuseIdentifier: PartnerTextCell.Identifier)
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
         collectionView.frame = view.frame
         collectionView.backgroundColor = UIColor.white
         
@@ -86,6 +124,7 @@ class PartnersViewController: UIViewControllerWithMenu, UICollectionViewDelegate
             return cell
         }
     }
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if partners[indexPath.row].cellType == .image {
             return CGSize(width: collectionView.frame.width, height: imageCellHeight)
@@ -97,9 +136,11 @@ class PartnersViewController: UIViewControllerWithMenu, UICollectionViewDelegate
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 50)
     }
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 100)
     }
